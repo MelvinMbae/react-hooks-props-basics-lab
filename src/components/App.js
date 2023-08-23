e5348e16fe18928fd258b258983c3ab8ed1c425c
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-08-23 12:03:33 +0300

    Lab Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,25 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
+
 // pass this data down as props to the child component(s) that need it!
+
 import user from "../data/user";
+
 console.log(user);
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home {...user} />
+      <About bio={user.bio} />
+
     </div>
   );
 }
 
 export default App;
+
